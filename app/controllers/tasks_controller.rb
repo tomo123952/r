a818bd21fe44f90f05b146b86d1b6419ca2e1aea
tom.kenji <surume265@gmail.com>
2021-05-11 06:01:13 +0000

    top page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,58 +1,34 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  
-  def index
-    @tasks = Task.order(id: :desc).page(params[:page]).per(10)
-  end
-  
-  def show
-  end
-  
-  def new
-    @task = Task.new
-  end
-  
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'Taskが正常に投稿されました'
-      redirect_to @task
+      flash[:success] = 'メッセージを投稿しました。'
+      redirect_to root_url
     else
-      flash.now[:danger] = 'Taskが投稿されませんでした'
-      render :new
-    end      
-  end
-  
-  def edit
-  end
-  
-  def update
-    if @task.update(task_params)
-      flash[:success] = 'Taskは正常に更新されました'
-      redirect_to @task
-    else
-      flash[:danger] = 'Taskは更新されませんでした'
-      render :edit
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      render 'tasks/index'
     end
   end
-  
+
   def destroy
     @task.destroy
-    
-    flash[:success] = 'Taskは正常に削除されました'
-    redirect_to tasks_url
+    flash[:success] = 'メッセージを削除しました。'
+    redirect_back(fallback_location: root_path)
   end
   
   private
   
   def task_params
-    params.require(:task).permit(:content, :status)
+    params.require(:task).permit(:content)
   end
   
-  def set_task
-     @task = Task.find(params[:id])
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
+  get 'tasks/create'
+  get 'tasks/destroy'
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
@@ -6,12 +9,12 @@ Rails.application.routes.draw do
   get 'users/show'
   get 'users/new'
   get 'users/create'
-  root to: 'tasks#index' 
+  root to: 'toppages#index' 
   
   get 'login', to:'sessions#new'
   post 'login', to:'sessions#create'
   delete 'logout', to:'sessions#destroy'
   get 'signup', to: 'users#new'
   resources :users, only:[:index, :show, :create]
-  resources :tasks
+  resources :tasks, only: [:create, :destroy]
 end
