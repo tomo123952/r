789a85755e396c6c596fb636c180531417cba461
tom.kenji <surume265@gmail.com>
2021-05-11 14:21:05 +0000

    kadai

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,13 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
   
-  def index
-    @users = User.order(id: :desc).page(params[:page]).per(25)
-  end
-
-  def show
-    @user = User.find(params[:id])
-    @tasks = @user.tasks.order(id: :desc).page(params[:page])
-  end
-
+  
   def new
     @user = User.new
   end
+  
+  def show
+end
 
   def create
     @user = User.new(user_params)
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,13 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
-  get 'users/index'
-  get 'users/show'
   get 'users/new'
   get 'users/create'
   root to: 'tasks#index' 
 
   
-  get 'login', to:'sessions#new'
-  post 'login', to:'sessions#create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
   delete 'logout', to:'sessions#destroy'
   get 'signup', to: 'users#new'
   resources :users, only:[:index, :show, :create]
