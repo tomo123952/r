88c93a4fe093b38779f10e2ec962b3647d387139
tom.kenji <surume265@gmail.com>
2021-05-11 10:35:32 +0000

    1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,32 @@ class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:destroy]
   
+  def index
+    @tasks = Task.all
+  end
+  
+  def new
+    @task = Task.new
+  end
+  def edit
+     @task = Task.find(params[:id])
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
+  end
   def create
     @task = current_user.tasks.build(task_params)
     if @task.save
@@ -15,9 +41,10 @@ class TasksController < ApplicationController
   end
 
   def destroy
+    @task = Task.find(params[:id])
     @task.destroy
-    flash[:success] = 'タスクを削除しました。'
-    redirect_back(fallback_location: root_path)
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
-  get 'tasks/create'
-  get 'tasks/destroy'
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
@@ -9,12 +6,13 @@ Rails.application.routes.draw do
   get 'users/show'
   get 'users/new'
   get 'users/create'
-  root to: 'toppages#index' 
+  root to: 'tasks#index' 
+
   
   get 'login', to:'sessions#new'
   post 'login', to:'sessions#create'
   delete 'logout', to:'sessions#destroy'
   get 'signup', to: 'users#new'
   resources :users, only:[:index, :show, :create]
-  resources :tasks, only: [:create, :destroy]
+  resources :tasks
 end
